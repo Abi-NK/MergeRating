87c98d36a447266cf72a72d19b4ef41a4752698f
Abhishek-NK <53003648+Abhishek-NK@users.noreply.github.com>
2020-07-22 18:43:03 +0100

    Implemented a merge sort algorithm to sort numbers.

--- app/src/main/java/com/example/mergerate/MainActivity2.kt
+++ app/src/main/java/com/example/mergerate/MainActivity2.kt
@@ -2,21 +2,65 @@ package com.example.mergerate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.DarkTheme)
         setTitle("Scientific Ranking")
         setContentView(R.layout.activity_main2)
 
         val movie = intent.getStringExtra("movie")
         print(movie + "hello")
-        textView2.setText(movie).toString()
 
+        if (movie.equals("StarWars")) {
+            //textViewQuestion.setText(movie).toString()
 
-
-
+            val numbers = mutableListOf(38,27,43,3,9,82,10)
+            val sortedList = mergeSort(numbers)
+            println("Unsorted: $numbers")
+            println("Sorted: $sortedList")
+        }
     }
+
+    fun mergeSort(list: List<Int>): List<Int> {
+        if (list.size <= 1) {
+            return list
+        }
+
+        val middle = list.size / 2
+        var left = list.subList(0,middle);
+        var right = list.subList(middle,list.size);
+
+        return merge(mergeSort(left), mergeSort(right))
+    }
+
+    private fun merge(left: List<Int>, right: List<Int>): List<Int>  {
+        var indexLeft = 0
+        var indexRight = 0
+        var newList : MutableList<Int> = mutableListOf()
+
+        while (indexLeft < left.count() && indexRight < right.count()) {
+            if (left[indexLeft] <= right[indexRight]) {
+                newList.add(left[indexLeft])
+                indexLeft++
+            } else {
+                newList.add(right[indexRight])
+                indexRight++
+            }
+        }
+
+        while (indexLeft < left.size) {
+            newList.add(left[indexLeft])
+            indexLeft++
+        }
+
+        while (indexRight < right.size) {
+            newList.add(right[indexRight])
+            indexRight++
+        }
+        return newList;
+    }
+
 }
