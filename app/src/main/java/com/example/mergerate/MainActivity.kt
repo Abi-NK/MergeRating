073fbff05e080908bbc1b590cf2c644caea8abc5
Abhishek-NK <53003648+Abhishek-NK@users.noreply.github.com>
2020-07-22 13:56:43 +0100

    Image button added for the first quiz

--- app/src/main/java/com/example/mergerate/MainActivity.kt
+++ app/src/main/java/com/example/mergerate/MainActivity.kt
@@ -4,15 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.DarkTheme)
+        setTitle("Scientific Ranking")
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.buttonActivity)
+        val image = findViewById<ImageButton>(R.id.imageButtonStarWars)
+        //val button = findViewById<Button>(R.id.buttonActivity)
 
-        button.setOnClickListener {
+        image.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
 
             startActivity(intent)
--- app/src/main/java/com/example/mergerate/MainActivity2.kt
+++ app/src/main/java/com/example/mergerate/MainActivity2.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTitle("Scientific Ranking")
         setContentView(R.layout.activity_main2)
     }
 }
