bd522704e7cf0dc42a9f200a509963616a0c3b40
Abhishek-NK <53003648+Abhishek-NK@users.noreply.github.com>
2020-07-22 14:45:30 +0100

    Added a second button for another movie

--- app/src/main/java/com/example/mergerate/MainActivity.kt
+++ app/src/main/java/com/example/mergerate/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +14,19 @@ class MainActivity : AppCompatActivity() {
         setTitle("Scientific Ranking")
         setContentView(R.layout.activity_main)
 
-        val image = findViewById<ImageButton>(R.id.imageButtonStarWars)
-        //val button = findViewById<Button>(R.id.buttonActivity)
+        val imageStarWars = findViewById<ImageButton>(R.id.imageButtonStarWars)
+        val imageHarryPotter = findViewById<ImageButton>(R.id.imageButtonHarryPotter)
 
-        image.setOnClickListener {
+        imageStarWars.setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
-
+            val movie = "StarWars"
+            intent.putExtra("movie", movie)
             startActivity(intent)
+        }
+
+        imageHarryPotter.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent);
         }
 
     }
